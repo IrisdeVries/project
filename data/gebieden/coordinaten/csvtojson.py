b5c2d1bb54b749b502d679d0be01a43ba24773ee
Iris de Vries <Iris@MacBook-Air-van-Iris.local>
2016-01-10 23:33:55 +0100

    csv->goede format

--- data/gebieden/coordinaten/csvtojson.py
+++ data/gebieden/coordinaten/csvtojson.py
@@ -18,22 +18,3 @@ for row in reader:
 	print"lng:"
 	print lng
 	print "},"
-
-
-
-        # tempor = []
-        # date = row[0][4:13]
-        # sdate = ''
-        # for char in date:
-        #     if len(sdate) == 0:
-        #         sdate += '/'
-        #     sdate += char
-        # tempor.append(sdate)
-
-        # # get temperature
-        # temp = row[1:2][0]
-        # tempor.append(temp)
-        # aList.append(tempor)
-
-# with open('formatted_nieuw_weer.txt', 'w') as fp:
-# 	json.dump(aList, fp)
